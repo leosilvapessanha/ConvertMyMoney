ef0317fe5b6fbab237471430fbea95eab1c42fec
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-06-09 16:17:16 -0300

    MVP

---
+++ README.md
@@ -0,0 +1,21 @@
+**Convert My Money!**
+
+
+#### Requirements
+
+* [NodeJs](https://nodejs.org/en/)
+
+* [Npm](https://docs.npmjs.com/cli/install)
+
+#### Dependencies
+
+* [EJS](https://www.npmjs.com/package/ejs)
+
+* [Express](https://www.npmjs.com/package/express)
+
+* [Jest](https://jestjs.io/docs/en/getting-started)
+	
+
+#### Running the project
+
+Inside the terminal, just type the command line _**" node index.js "**_.
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const convert = require('./lib/convert')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+
+})
+
+app.get('/cotacao', (req, res) => {    
+    const  {cotacao,quantidade } = (req.query)      
+    if ((cotacao  && quantidade  )) {                
+        const conversao = convert.convert(cotacao, quantidade)        
+        res.render('cotacao', {
+            error:false,
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao)
+        
+        })
+        }else{
+            res.render('cotacao',{
+             error: 'Valores Inválidos'        
+              
+          
+        })
+        }
+    
+})
+
+
+app.listen(3000, err => {
+    if (err) {
+        console.log('Não foi possivel iniciar')
+    } else {
+        console.log('Servidor rodando na porta 3000')
+    }
+
+})
+// const express = require('express')
+// const app = express()
+// const path = require('path')
+// const convert = require('./lib/convert')
+
+// app.set('view engine', 'ejs')
+// app.set('views', path.join(__dirname, 'views'))
+// app.use(express.static(path.join(__dirname, 'public')))
+
+//app.get('/cotacao', (req, res) => { 
+//   const { cotacao, quantidade } = req.query
+//   console.log(cotacao, quantidade)
+//   if(cotacao && quantidade){
+//       const conversao = convert.convert(cotacao, quantidade)
+//       res.render('cotacao', {
+//           error: false,
+//           cotacao: convert.toMoney(cotacao),
+//           quantidade: convert.toMoney(quantidade),
+//           conversao: convert.toMoney(conversao)
+//       })
+//   }else{
+//       res.render('cotacao', {
+//           error: 'Valores inválidos'
+//       })
+//   }
+// })
+
+
+  
+
+
+
+
+// app.listen(3000, err =>{
+//   if(err){
+//     console.log('ops, something does not feel right')
+//   }
+//   else{
+//     console.log('all good!')
+//   }
+// })
